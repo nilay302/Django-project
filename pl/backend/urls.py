efe31f5b6f01479eea7ba5f612f8733fac720cd8
nilay302 <nilayshirke52@gmail.com>
2022-12-06 10:50:52 +0530

    login and register backend

--- pl/backend/urls.py
+++ pl/backend/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('tweetsList', views.list,name="tweetList"),
     path('tweetDetail/<int:pk>', views.details),
     path('createTweet/',views.CreateTweets,name="CreateTweet"),
+    path('register/',views.UserRegister,name="Register"),
 ]
--- pl/backend/views.py
+++ pl/backend/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Tweets
 from django.http import Http404
-from .forms import TweetForm
+from .forms import TweetForm, CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def list(request):
     all_tweets = Tweets.objects.all()
@@ -14,6 +19,24 @@ def details(request,pk):
         raise Http404('Tweet Does not exist')
     return render(request,'backend/tweet_detail.html',{'tweet':tweet})
 
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username = username, password = password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('tweetList')
+    
+    return render(request,'backend/login.html',{})
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def CreateTweets(request):
     form = TweetForm()
     if request.method == 'POST':
@@ -23,3 +46,15 @@ def CreateTweets(request):
             return redirect('tweetList')
     context ={'form':form}
     return render(request,'backend/tweet_form.html',context)
+
+def UserRegister(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Account Created')
+            return redirect('tweetList')
+    context = {'form':form}
+    return render(request,'backend/register.html',context)
+   
